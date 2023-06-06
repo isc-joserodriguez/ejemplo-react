26c0935ffb59c276f3970f1437e0927394c65df6
Jose Rodriguez <j.rodriguez@blackstone.studio>
2023-06-05 20:37:51 -0700

    Ejemplo React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import ComponenteImportado from './otroNombre/Componente';
 
 function App() {
+  const personas = [
+    {
+      nombre: 'Mario',
+      edad: '10',
+    },
+    {
+      nombre: 'Pedro',
+      edad: '80',
+    },
+    {
+      nombre: 'Ramona',
+      edad: '20',
+    },
+    {
+      nombre: 'Paola',
+      edad: '5',
+    },
+    {
+      nombre: 'Luis',
+      edad: '60',
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola, esta es mi primera App!!!!!</h1>
+      <a href="http://www.google.com">Ir a Google!</a>
+      <br />
+      {/* <ComponenteImportado nombre="Rocío" edad="30" /> 
+      <ComponenteImportado nombre="Mario" edad="20" /> 
+  <ComponenteImportado nombre="Julio" edad="10" />  */}
+      {personas.map((persona) => (
+        <div>
+          <h1>Mensaje</h1>
+          <ComponenteImportado nombre={persona.nombre} edad={persona.edad} />
+          {/* Props */}
+        </div>
+      ))}
     </div>
   );
 }
+
+// SPA
+// Single Page Application
 
 export default App;
---
+++ src/otroNombre/Componente.js
@@ -0,0 +1,10 @@
+//JSX
+const Componente = ({ nombre, edad }) => {
+  return (
+    <div>
+      {nombre} tienes {edad} años. Hola, este es un componente.
+    </div>
+  );
+};
+
+export default Componente;
